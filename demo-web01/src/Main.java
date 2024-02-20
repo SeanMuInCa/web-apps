3a571740342341c5d84d5505374e75db47cb22ab
Sean <fengying1028@hotmail.com>
2024-02-20 12:55:10 -0600

    webapp

---
+++ demo-web01/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Purpose:             <br />
+ * Data Submitted:      2024/2/20 <br />
+ * Assignment Number:    <br />
+ * Course Name:         COSC602  <br />
+ * Instructor:          George Ding  <br />
+ * File Path:           <br />
+ *
+ * @author Zhenghua Mu
+ * @version 1.0.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
